f8263f27bb1f8d153d4aba1db26b6ee4e3ba748a
danushka96 <danushkaherath96@gmail.com>
2019-07-30 23:47:28 +0530

    add basic crypto functions

--- pitcoin.py
+++ pitcoin.py
@@ -21,6 +21,7 @@ class Blockchain:
         self.chain = []
         self.transactions = []
         self.create_block(proof = 1, previous_hash = '0')
+        self.nodes = set()
         
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain)+1,
@@ -72,6 +73,27 @@ class Blockchain:
                                   'amount': amount})
         previous_block = self.get_previous_block();
         return previous_block['index'] + 1;
+    
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+        
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for nodes in network:
+            response = requests.get(f'http://{nodes}/get_chain')
+            if response.status_code == '200':
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length>max_length and self.is_valid_chain(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
     
 # Creating the web App
 app = Flask(__name__)
